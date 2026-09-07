ddee9627e6fc02d352c9b154fc497b8d94eb3e08
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-07 16:45:46 +0400

    Task 351 - finish inventory report

--- src/FileInventoryReport.java
+++ src/FileInventoryReport.java
@@ -146,5 +146,7 @@ public class FileInventoryReport {
         } else {
             System.out.println("Largest File     : None");
         }
+
+        System.out.println("Inventory report completed.");
     }
 }
